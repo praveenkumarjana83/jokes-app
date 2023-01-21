ec748602ab8bee2aa7a42706de047ef67acbd2d0
Praveen Kumar Jana <praveenkumarjana83@gmail.com>
2023-01-21 16:25:45 +0800

    added service

---
+++ src/main/java/com/jpk/jokesapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.jpk.jokesapp.services;
+
+public interface JokeService {
+
+    public String  getJoke();
+}
---
+++ src/main/java/com/jpk/jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,16 @@
+package com.jpk.jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public  JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
