801e621b450d2d6763db2098e49d9b96fd80c1b7
Praveen Kumar Jana <praveenkumarjana83@gmail.com>
2023-01-21 15:59:33 +0800

    initial commit

---
+++ src/test/java/com/jpk/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpk.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
