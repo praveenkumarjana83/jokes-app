ffce34d241515e4bff03ce0e622145a2841afda7
Praveen Kumar Jana <praveenkumarjana83@gmail.com>
2023-01-22 10:44:58 +0800

    added controller &  view

--- src/main/java/com/jpk/jokesapp/controller/JokeController.java
+++ src/main/java/com/jpk/jokesapp/controller/JokeController.java
@@ -1,2 +1,24 @@
-package com.jpk.jokesapp.controller;public class JokeController {
+package com.jpk.jokesapp.controller;
+
+import com.jpk.jokesapp.services.JokeService;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/"})
+    public  String showJoke(@NotNull Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+
 }
